37f3f3363f9eabe3a7bfb99d25b7a6a060fcf263
vintagepc <53943260+vintagepc@users.noreply.github.com>
2020-04-09 22:09:01 -0400

    More 2130 work

--- board_einsy/hwDefs/TMC2130.c
+++ board_einsy/hwDefs/TMC2130.c
@@ -6,49 +6,154 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "sim_avr.h"
+#include "avr_spi.h"
+#include "avr_adc.h"
 #include "TMC2130.h"
 
-#define TRACE(_w) _w
+//#define TRACE(_w) _w
 #ifndef TRACE
 #define TRACE(_w)
 #endif
 
+
+static void tmc2130_create_reply(tmc2130_t *this)
+{
+    if (!this->cmdOut.bitsIn.RW) // Last in was a read.
+    {
+        this->cmdOut.bitsOut.data = this->regs.raw[this->cmdOut.bitsIn.address];
+    }
+    // If the last was a write, the old data is left intact.
+
+    // Set the status bits on the reply:
+    this->cmdOut.bitsOut.driver_error = this->regs.defs.GSTAT.drv_err;
+    this->cmdOut.bitsOut.reset_flag = this->regs.defs.GSTAT.reset;
+    this->cmdOut.bitsOut.sg2 = this->regs.defs.DRV_STATUS.stallGuard;
+    this->cmdOut.bitsOut.standstill = this->regs.defs.DRV_STATUS.stst;
+    TRACE(printf("Reply built: %10lx\n",this->cmdOut.all));
+}
+
+// Called when a full command is ready to process. 
+static void tmc2130_process_command(tmc2130_t *this)
+{
+    TRACE(printf("tmc2130 %c cmd: w: %x a: %02x  d: %08x\n",this->axis, this->cmdIn.bitsIn.RW, this->cmdIn.bitsIn.address, this->cmdIn.bitsIn.data));
+    if (this->cmdIn.bitsIn.RW)
+        this->regs.raw[this->cmdIn.bitsIn.address] = this->cmdIn.bitsIn.data;
+    else
+    {
+        TRACE(printf("Read command on register: %02x\n", this->cmdIn.bitsIn.address));
+    }
+    tmc2130_create_reply(this);
+    this->cmdOut = this->cmdIn;
+}
+
 /*
- * called when a SPI byte is sent
+ * called when a SPI byte is received
  */
 static void tmc2130_spi_in_hook(struct avr_irq_t * irq, uint32_t value, void * param)
 {
-	tmc2130_t* this = (tmc2130_t*)param;
+    tmc2130_t* this = (tmc2130_t*)param;
     if (!this->flags.bits.selected)
         return;
+    // Clock out a reply byte:
+    uint8_t byte = this->cmdOut.bytes[0];
+    this->cmdOut.all<<=8;
+    avr_raise_irq(this->irq + IRQ_TMC2130_SPI_BYTE_OUT,byte);
+    TRACE(printf("TMC2130 %c: Clocking out %02x\n",this->axis,byte));
+
 	TRACE(printf("TMC2130 %c: byte received: %02x\n",this->axis,value));
+    this->cmdIn.all<<=8; // Shift bits up
+    this->cmdIn.bytes[4] = value;
+    
+
+
 }
 
+// Called when the MCU wants to read the diag pin ADC
+static void tmc2130_diag_read_hook(struct avr_irq_t * irq, uint32_t value, void * param)
+{
+    tmc2130_t* this = (tmc2130_t*)param;
+    TRACE(printf("TMC2130 %c: Diag pin read\n",this->axis));
+    avr_raise_irq(this->irq + IRQ_TMC2130_DIAG_OUT,0xFFFFFF);
+}
 // Called when CSEL changes.
 static void tmc2130_csel_in_hook(struct avr_irq_t * irq, uint32_t value, void * param)
 {
     tmc2130_t* this = (tmc2130_t*)param;
-	TRACE(printf("TMC2130 %c: CSEL changed to %02x\n",this->axis,value));
+	//TRACE(printf("TMC2130 %c: CSEL changed to %02x\n",this->axis,value));
     this->flags.bits.selected = (value==0); // NOTE: active low!
+    if (value == 1) // Just finished a CSEL
+        tmc2130_process_command(this);
+}
+
+// Called when DIR pin changes.
+static void tmc2130_dir_in_hook(struct avr_irq_t * irq, uint32_t value, void * param)
+{
+    tmc2130_t* this = (tmc2130_t*)param;
+	TRACE(printf("TMC2130 %c: DIR changed to %02x\n",this->axis,value));
+    this->flags.bits.dir = value;
+}
+
+// Called when STEP is triggered.
+static void tmc2130_step_in_hook(struct avr_irq_t * irq, uint32_t value, void * param)
+{
+    tmc2130_t* this = (tmc2130_t*)param;
+	TRACE(printf("TMC2130 %c: STEP changed to %02x\n",this->axis,value));
+    if (this->flags.bits.dir)
+        TRACE(printf("TMC2130 %c: Step++\n",this->axis));
+    else
+        TRACE(printf("TMC2130 %c: Step--\n",this->axis));
+}
+
+// Called when DRV_EN is triggered.
+static void tmc2130_enable_in_hook(struct avr_irq_t * irq, uint32_t value, void * param)
+{
+    tmc2130_t* this = (tmc2130_t*)param;
+	TRACE(printf("TMC2130 %c: EN changed to %02x\n",this->axis,value));
+    this->flags.bits.enable = value==0; // active low, i.e motors off when high.
 }
 
 static const char * irq_names[IRQ_TMC2130_COUNT] = {
 		[IRQ_TMC2130_SPI_BYTE_IN] = "8<tmc2130.spi.in",
 		[IRQ_TMC2130_SPI_BYTE_OUT] = "8>tmc2130.chain",
         [IRQ_TMC2130_SPI_COMMAND_IN] = "40<tmc2130.cmd",
-        [IRQ_TMC2130_SPI_CSEL] = "tmc2130.csel"
+        [IRQ_TMC2130_SPI_CSEL] = "tmc2130.csel",
+        [IRQ_TMC2130_STEP_IN] = "tmc2130.step",
+        [IRQ_TMC2130_DIR_IN] = "tmc2130.dir",
+        [IRQ_TMC2130_ENABLE_IN] = "tmc2130.enable",
+        [IRQ_TMC2130_DIAG_OUT] = "tmc2130.diag",
+        [IRQ_TMC2130_DIAG_TRIGGER_IN] = "tmc2130.diagReq"
 };
 
 void
 tmc2130_init(
 		struct avr_t * avr,
 		tmc2130_t *p,
-        char axis)
+        char axis, uint8_t iDiagADC) 
 {
 	p->irq = avr_alloc_irq(&avr->irq_pool, 0, IRQ_TMC2130_COUNT, irq_names);
     p->axis = axis;
+    memset(&p->cmdIn, 0, sizeof(p->cmdIn));
+    p->byteIndex = 4;
+
+    // Just wire right up to the AVR SPI
+    avr_connect_irq(avr_io_getirq(avr,AVR_IOCTL_SPI_GETIRQ(0),SPI_IRQ_OUTPUT),
+		p->irq + IRQ_TMC2130_SPI_BYTE_IN);
+    avr_connect_irq(p->irq + IRQ_TMC2130_SPI_BYTE_OUT,
+        avr_io_getirq(avr,AVR_IOCTL_SPI_GETIRQ(0),SPI_IRQ_INPUT));
+    
+	avr_irq_t * src = avr_io_getirq(avr, AVR_IOCTL_ADC_GETIRQ, ADC_IRQ_OUT_TRIGGER);
+	avr_irq_t * dst = avr_io_getirq(avr, AVR_IOCTL_ADC_GETIRQ, iDiagADC);
+	if (src && dst) {
+		avr_connect_irq(src, p->irq + IRQ_TMC2130_DIAG_TRIGGER_IN);
+		avr_connect_irq(p->irq + IRQ_TMC2130_DIAG_OUT, dst);
+    }
 	avr_irq_register_notify(p->irq + IRQ_TMC2130_SPI_BYTE_IN, tmc2130_spi_in_hook, p);
     avr_irq_register_notify(p->irq + IRQ_TMC2130_SPI_CSEL, tmc2130_csel_in_hook, p);
+    avr_irq_register_notify(p->irq + IRQ_TMC2130_DIR_IN, tmc2130_dir_in_hook, p);
+    avr_irq_register_notify(p->irq + IRQ_TMC2130_STEP_IN, tmc2130_step_in_hook, p);
+    avr_irq_register_notify(p->irq + IRQ_TMC2130_ENABLE_IN, tmc2130_enable_in_hook, p);
+    avr_irq_register_notify(p->irq + IRQ_TMC2130_DIAG_TRIGGER_IN, tmc2130_diag_read_hook,p);
 }
 
--- board_einsy/hwDefs/TMC2130.h
+++ board_einsy/hwDefs/TMC2130.h
@@ -19,8 +19,11 @@ enum {
 	IRQ_TMC2130_SPI_BYTE_OUT,		// to chain them !!
     IRQ_TMC2130_SPI_COMMAND_IN, // full 40-bit command
     IRQ_TMC2130_SPI_CSEL,
-    //IRQ_TMC2130_STEP_IN,
-    //IRQ_TMC2130_DIR_IN,
+    IRQ_TMC2130_STEP_IN,
+    IRQ_TMC2130_DIR_IN,
+    IRQ_TMC2130_ENABLE_IN,
+    IRQ_TMC2130_DIAG_OUT,
+    IRQ_TMC2130_DIAG_TRIGGER_IN,
     //IRQ_TMC2130_COIL_A1_OUT,
     //IRQ_TMC2130_COIL_A2_OUT,
     //IRQ_TMC2130_COIL_B1_OUT,
@@ -33,40 +36,76 @@ typedef union tmc2130_flags_t{
     struct 
     {
         uint8_t selected : 1;
+        uint8_t dir : 1;
+        uint8_t enable :1;
     } bits;
 } tmc2130_flags_t;
 
-typedef union tmc2130_cmdIn_t{
-    uint8_t bytes[5]; // Raw bytes as piped in by SPI.
+typedef union tmc2130_cmd_t{
+    uint8_t bytes[5]; // Raw bytes as piped in/out by SPI.
     struct {
-        uint8_t RW :1;
-        uint8_t address :7;
         unsigned long data :32; // 32 bits of data
-    } bits
-} tmc2130_cmdIn_t;
-
-typedef union tmc2130_cmdOut_t{
-    uint8_t bytes[5]; // Raw bytes as piped in by SPI.
+        uint8_t address :7;
+        uint8_t RW :1;
+    } bitsIn;
     struct {
-        uint8_t :5; // unused
-        uint8_t standstill :1;
-        uint8_t sg2 :1;
-        uint8_t driver_error :1;
-        uint8_t reset_flag :1;
         unsigned long data; // 32 bits of data
-    } bits
-} tmc2130_cmdOut_t;
+        uint8_t reset_flag :1;
+        uint8_t driver_error :1;
+        uint8_t sg2 :1;
+        uint8_t standstill :1;
+        uint8_t :5; // unused
+    } bitsOut;
+    uint64_t all :40;
+} tmc2130_cmd_t;
+
+// the internal programming registers.
+typedef union
+{
+    uint32_t raw[128]; // There are 128, 7-bit addressing.
+    // TODO: add fields for specific ones down the line...
+    struct {
+        uint32_t GCONF;             // 0x00
+        struct                 // 0x01
+        {
+            uint8_t reset   :1;
+            uint8_t drv_err :1;
+            uint8_t uv_cp   :1;
+        } GSTAT;
+        uint32_t _undefined[109];   // 0x02 - 0x6E
+        struct                      //0x6F
+        {
+            uint16_t SG_RESULT   :10;
+            uint8_t             :5;
+            uint8_t fsactive    :1;
+            uint8_t CS_ACTUAL   :5;
+            uint8_t             :3;
+            uint8_t stallGuard  :1;
+            uint8_t ot          :1;
+            uint8_t otpw        :1;
+            uint8_t sg2a        :1;
+            uint8_t sg2b        :1;
+            uint8_t ola         :1;
+            uint8_t olb         :1;
+            uint8_t stst        :1;
+        }DRV_STATUS;
+    }defs;
+} tmc2130_registers_t;
 
 typedef struct tmc2130_t {
 	avr_irq_t *	irq;		// irq list
     char axis; // Useful for print debugging.
 	tmc2130_flags_t flags;
+    int8_t byteIndex;
+    tmc2130_cmd_t cmdIn;
+    tmc2130_cmd_t cmdOut; // the previous data for output.
+    tmc2130_registers_t regs;
 	// TODO...
 } tmc2130_t;
 
 void
 tmc2130_init(
 		struct avr_t * avr,
-		tmc2130_t *p, char axis);
+		tmc2130_t *p, char axis, uint8_t iDiagADC);
 
 #endif

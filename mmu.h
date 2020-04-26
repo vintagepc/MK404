1d6b58654ffcaf6b3a583fb9507143d01c6ecd17
vintagepc <53943260+vintagepc@users.noreply.github.com>
2020-04-25 22:02:46 -0400

    Mostly working

--- mmu.h
+++ mmu.h
@@ -10,18 +10,45 @@
 #include <pthread.h>
 #include <uart_pty.h>
 #include "hc595.h"
+#include "mmu_buttons.h"
+#include "led.h"
 #include "TMC2130.h"
 
 typedef struct mmu_t{
     avr_t *avr;
     bool bQuit;
     bool bStarted;
+    bool bReset;
     pthread_t run;
     uart_pty_t UART0;
     uart_pty_t *UARTHost;
-    hc595_t shiftMotors;
+    hc595_t shift;
     tmc2130_t Sel, Idl, Extr;
+    led_t lGreen[5], lRed[5];
+    buttons_t buttons;
 } mmu_t;
+
+typedef union hc595_data_t{
+    uint32_t raw;
+    struct {
+        uint8_t b0 :1;
+        uint8_t b1 :1;
+        uint8_t b2 :1;
+        uint8_t b3 :1;
+        uint8_t b4 :1;
+        uint8_t b5 :1;
+        uint8_t b6 :1;
+        uint8_t b7 :1;
+        uint8_t b8 :1;
+        uint8_t b9 :1;
+        uint8_t b10 :1;
+        uint8_t b11 :1;
+        uint8_t b12 :1;
+        uint8_t b13 :1;
+        uint8_t b14 :1;
+        uint8_t b15 :1;
+    }bits;
+}hc595_data_t;
 
 mmu_t* mmu_init(avr_t *host, avr_irq_t *hostTX, avr_irq_t *hostRX, avr_irq_t *irqReset);
 

922ff9d89654d3f16a7c32db57f196563c1f2d82
vintagepc <53943260+vintagepc@users.noreply.github.com>
2020-04-30 21:43:20 -0400

    Shift reg

---
+++ hwDefs/HC595.h
@@ -0,0 +1,99 @@
+/*
+	HC595.h - a shift register for the MMU.
+
+	Original SPI portion Copyright 2008, 2009 Michel Pollet <buserror@gmail.com>
+
+	Rewritten for C++ and a bitbanged connection in 2020, VintagePC <https://github.com/vintagepc/>
+
+ 	This file is part of MK3SIM.
+
+	MK3SIM is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	MK3SIM is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with MK3SIM.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __HC595_H__
+#define __HC595_H__
+
+#include "BasePeripheral.h"
+
+/*
+ * this one is quite fun, it simulated a 74HC595 shift register
+ * driven by an SPI signal.
+ * For the interest of the simulation, they can be chained, but 
+ * for practicality sake the shift register is kept 32 bits
+ * wide so it acts as 4 of them "daisy chained" already. 
+ */
+
+class HC595: public BasePeripheral
+{
+	public:
+
+		#define IRQPAIRS \
+			_IRQ(SPI_BYTE_IN,"8<hc959.spi_in") \
+			_IRQ(SPI_BYTE_OUT,"8>hc959.spi_out") \
+			_IRQ(IN_LATCH,"32<hc959.latch_in") \
+			_IRQ(IN_RESET,"<hc959.reset_in") \
+			_IRQ(IN_CLOCK,"<hc959.clock_in") \
+			_IRQ(IN_DATA,"<hc959.data_in") \
+			_IRQ(OUT,"32>hc959.out") \
+			_IRQ(BIT0	,">bit0") \
+			_IRQ(BIT1	,">bit1") \
+			_IRQ(BIT2	,">bit2") \
+			_IRQ(BIT3	,">bit3") \
+			_IRQ(BIT4	,">bit4") \
+			_IRQ(BIT5	,">bit5") \
+			_IRQ(BIT6	,">bit6") \
+			_IRQ(BIT7	,">bit7") \
+			_IRQ(BIT8	,">bit8") \
+			_IRQ(BIT9	,">bit9") \
+			_IRQ(BIT10	,">bit10") \
+			_IRQ(BIT11	,">bit11") \
+			_IRQ(BIT12	,">bit12") \
+			_IRQ(BIT13	,">bit13") \
+			_IRQ(BIT14	,">bit14") \
+			_IRQ(BIT15	,">bit15") \
+			_IRQ(BIT16	,">bit16") \
+			_IRQ(BIT17	,">bit17") \
+			_IRQ(BIT18	,">bit18") \
+			_IRQ(BIT19	,">bit19") \
+			_IRQ(BIT20	,">bit20") \
+			_IRQ(BIT21	,">bit21") \
+			_IRQ(BIT22	,">bit22") \
+			_IRQ(BIT23	,">bit23") \
+			_IRQ(BIT24	,">bit24") \
+			_IRQ(BIT25	,">bit25") \
+			_IRQ(BIT26	,">bit26") \
+			_IRQ(BIT27	,">bit27") \
+			_IRQ(BIT28	,">bit28") \
+			_IRQ(BIT29	,">bit29") \
+			_IRQ(BIT30	,">bit30") \
+			_IRQ(BIT31	,">bit31")
+		#include "IRQHelper.h"
+
+		void Init(avr_t *avr);
+
+	private:
+
+		void OnLatchIn(avr_irq_t *irq, uint32_t value);
+		void OnClockIn(avr_irq_t *irq, uint32_t value);
+		void OnSPIIn(avr_irq_t *irq, uint32_t value);
+		void OnDataIn(avr_irq_t *irq, uint32_t value);
+		void OnResetIn(avr_irq_t *irq, uint32_t value);
+
+		uint32_t	m_uiLatch = 0;		// value "on the pins"
+		uint32_t 	m_uiValue = 0;		// value shifted in
+		uint8_t		m_uiCurBit =0;
+		uint8_t		m_uiCurByte = 0;
+
+};
+#endif

d3d242199cfd18448d84567f80053dc8192f9aa0
vintagepc <53943260+vintagepc@users.noreply.github.com>
2020-05-09 12:51:08 -0400

    absolute hackjob attmept at getting objs rendered.

--- utility/TestVis.h
+++ utility/TestVis.h
@@ -1,15 +1,17 @@
 
 #include <vector>
+#include <type_traits>
 #include <tiny_obj_loader.h>
 #include <GL/glew.h>
-
-
 
 class TestVis 
 {
     public:
+        TestVis();
         void Draw();
         void Load();
+        void MouseCB(int button, int state, int x, int y);
+        void MotionCB(int x, int y, int iWin);
     private:
         typedef struct {
             GLuint vb;  // vertex buffer
@@ -21,6 +23,15 @@ class TestVis
         std::vector<tinyobj::material_t> materials;
         std::map<std::string, GLuint> textures;
 
+        int height = 800, width = 800;
+
+        bool bLoaded = false;
+        double prevMouseX, prevMouseY;
+        bool mouseLeftPressed;
+        bool mouseMiddlePressed;
+        bool mouseRightPressed;
+        float curr_quat[4];
+        float prev_quat[4];
         float eye[3], lookat[3], up[3], maxExtent;
         void _Draw(const std::vector<TestVis::DrawObject>& drawObjects, std::vector<tinyobj::material_t>& materials, std::map<std::string, GLuint>& textures); 
         bool LoadObjAndConvert(float bmin[3], float bmax[3],

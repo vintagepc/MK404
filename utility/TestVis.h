8c11f81c7d245da9ed5fe19834df1ceaeb5824c3
vintagepc <53943260+vintagepc@users.noreply.github.com>
2020-05-09 11:03:06 -0400

    tinkering

--- utility/TestVis.h
+++ utility/TestVis.h
@@ -9,15 +9,20 @@ class TestVis
 {
     public:
         void Draw();
+        void Load();
     private:
         typedef struct {
             GLuint vb;  // vertex buffer
             int numTriangles;
             size_t material_id;
         } DrawObject;
-
+        float bmin[3], bmax[3];
         std::vector<DrawObject> gDrawObjects;
+        std::vector<tinyobj::material_t> materials;
+        std::map<std::string, GLuint> textures;
 
+        float eye[3], lookat[3], up[3], maxExtent;
+        void _Draw(const std::vector<TestVis::DrawObject>& drawObjects, std::vector<tinyobj::material_t>& materials, std::map<std::string, GLuint>& textures); 
         bool LoadObjAndConvert(float bmin[3], float bmax[3],
                        std::vector<DrawObject>* drawObjects,
                        std::vector<tinyobj::material_t>& materials,

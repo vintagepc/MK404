88237658f6ea92af64c44bca0d4a5cf787d14b39
vintagepc <53943260+vintagepc@users.noreply.github.com>
2020-06-10 21:05:20 -0400

    Multicolor GL prints(#132)

--- utility/GLPrint.cpp
+++ utility/GLPrint.cpp
@@ -26,8 +26,8 @@
 
 static constexpr int iPrintRes = 100000; //0.1mm (meters/this)
 
-GLPrint::GLPrint()
-{	
+GLPrint::GLPrint(float fR, float fG, float fB):m_fColR(fR),m_fColG(fG),m_fColB(fB)
+{
 	Clear();
 }
 
@@ -40,18 +40,22 @@ void GLPrint::Clear()
 	m_fvDraw.clear();
 	m_fvNorms.clear();
 	m_bExtruding = false;
-	m_fEMax = 0;
+	m_fEMax = -1;
 }
 
 void GLPrint::NewCoord(float fX, float fY, float fZ, float fE)
 {
-	
+	if (m_fEMax<0) // First cycle/extrusion.
+	{
+		m_fEMax = fE;
+		m_fExtrEnd = m_fExtrStart = {fX,fZ,fY,fE};
+	}
 	int iX = fX*iPrintRes,iY = fY*iPrintRes,iZ = fZ*iPrintRes;
 	std::array<float,3> vfPos = {fX,fZ,fY};
-	// Test if the new coordinate is still collinear with the existing segment. 
+	// Test if the new coordinate is still collinear with the existing segment.
 	// Triangle area method. Slopes have risks of zero/inf/nan for very small deltas.
- 	//Ax(By - Cy) + Bx(Cy - Ay) + Cx(Ay - By) 
-	 // We don't bother with div by 2/abs since we only care if the area is 0. 
+ 	//Ax(By - Cy) + Bx(Cy - Ay) + Cx(Ay - By)
+	 // We don't bother with div by 2/abs since we only care if the area is 0.
 	int iArea = (m_iExtrStart[0]* ( m_iExtrEnd[2] - iY)) + (m_iExtrEnd[0]*(iY - m_iExtrStart[2])) + (iX * (m_iExtrStart[2] - m_iExtrEnd[2]));
 	float fArea = (m_fExtrStart[0]* ( m_fExtrEnd[2] - fY)) + (m_fExtrEnd[0]*(fY - m_fExtrStart[2])) + (fX * (m_fExtrStart[2] - m_fExtrEnd[2]));
 	if (fArea<0) fArea = -fArea;
@@ -74,7 +78,7 @@ void GLPrint::NewCoord(float fX, float fY, float fZ, float fE)
 	}
 	if (bExtruding ^ m_bExtruding)
 	{
-		// Extruding condition has changed. Start a new segment. 
+		// Extruding condition has changed. Start a new segment.
 		if (bExtruding) // Just started extruding. Update the various pointers.
 		{
 			m_iExtrStart = m_iExtrEnd;
@@ -84,7 +88,7 @@ void GLPrint::NewCoord(float fX, float fY, float fZ, float fE)
 			if (fZ>m_fCurZ)
 			{
 				//printf("New Z layer: %.02f\n", fZ);
-				//m_fLastZ = m_fCurZ; 
+				//m_fLastZ = m_fCurZ;
 				//m_fCurZ = fZ;
 			}
 			//printf("New extrusion %u at index %u\n",m_ivStart.size(),m_ivStart.back());
@@ -110,7 +114,7 @@ void GLPrint::NewCoord(float fX, float fY, float fZ, float fE)
 		}
 		m_bExtruding = bExtruding;
 	}
-	else if (!bColinear) 
+	else if (!bColinear)
 	{
 		// New segment, push it onto the vertex list and update the segment count
 		//printf("New segment: %d\n",m_vCoords.size());
@@ -126,7 +130,7 @@ void GLPrint::NewCoord(float fX, float fY, float fZ, float fE)
 		Normalize(fCross);
 		pfPrev[0] +=fCross[0]; pfPrev[1] += fCross[1]; pfPrev[2] += fCross[2];
 		pfPrev[0]/=2.f; pfPrev[1]/=2.f; pfPrev[2]/=2.f;
-		// And then append the new temporary end one. 
+		// And then append the new temporary end one.
 		std::transform(vfPos.begin(), vfPos.end(), m_fExtrEnd.data(), fA, std::minus<float>());
 		CrossProduct(fA,fB,fCross);
 		Normalize(fCross);
@@ -142,22 +146,22 @@ void GLPrint::NewCoord(float fX, float fY, float fZ, float fE)
 	m_fExtrEnd[0] = fX;
 	m_fExtrEnd[2] = fY;
 	m_fExtrEnd[1] = fZ;
-	m_iExtrEnd[0] = iX; 
+	m_iExtrEnd[0] = iX;
 	m_iExtrEnd[2] = iY;
 	m_iExtrEnd[1] = iZ;
 	//m_iExtrEnd[3] = iE;
-	
+
 }
 
 void GLPrint::Draw()
 {
-	float fColor[4] = {0.8,0,0,1};
+	float fColor[4] = {m_fColR,m_fColG,m_fColB,1};
 	float fG[4] = {0,0.5,0,1};
 	float fY[4] = {1,1,0,1};
 	float fK[4] = {0,0,0,1};
 	float fSpec[4] = {1,1,1,1};
 	glLineWidth(1.0);
-	
+
 	glMaterialfv(GL_FRONT_AND_BACK,GL_SPECULAR,fSpec);
 	glMaterialf(GL_FRONT_AND_BACK,GL_SHININESS,64);
 	//glEnable(GL_AUTO_NORMAL);
@@ -175,7 +179,7 @@ void GLPrint::Draw()
 		glMaterialfv(GL_FRONT_AND_BACK,GL_AMBIENT_AND_DIFFUSE,fSpec);
 		if (m_ivCount.size()>0)
 			glDrawArrays(GL_LINE_STRIP,m_ivStart.back(),((m_fvDraw.size()/3)-m_ivStart.back())-1);
-		if (m_bExtruding && m_fvDraw.size() >0) 
+		if (m_bExtruding && m_fvDraw.size() >0)
 		{
 			glMaterialfv(GL_FRONT_AND_BACK,GL_AMBIENT_AND_DIFFUSE,fY);
 			glBegin(GL_LINES);
@@ -183,7 +187,7 @@ void GLPrint::Draw()
 				glVertex3fv(m_fExtrEnd.data());
 			glEnd();
 		}
-		// Uncomment for vertex debugging. 
+		// Uncomment for vertex debugging.
 		 glMaterialfv(GL_FRONT_AND_BACK,GL_AMBIENT_AND_DIFFUSE,fG);
 		// glPointSize(1.0);
 		// glDrawArrays(GL_POINTS,0,m_fvDraw.size()/3);

710833e741231c3291fe4253f6eeee9c5b2d369b
vintagepc <53943260+vintagepc@users.noreply.github.com>
2020-07-03 09:57:03 -0400

    fix script bug

--- ref/updateTOC.sh
+++ ref/updateTOC.sh
@@ -2,11 +2,13 @@
 # Update scripting:
 echo "# Select a printer to view scripting options:" > Scripting.md
 for i in Scripting_*.md; do
-	echo "- [`basename ${i:10} .md`]($i)" >> Scripting.md
+	TMP=`basename $i .md`;
+	echo "- [${TMP:10}]($i)" >> Scripting.md
 done
 
 # Update trace:
 echo "# Select a printer to view trace options:" > TraceOptions.md
 for i in TraceOptions_*.md; do
-	echo "- [`basename ${i:13} .md`]($i)" >> TraceOptions.md
+	TMP=`basename $i .md`;
+	echo "- [${TMP:13}]($i)" >> TraceOptions.md
 done

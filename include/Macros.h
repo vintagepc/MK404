419efc46a0e0e53dfaafaabf50ff3a89a67dbd0f
vintagepc <53943260+vintagepc@users.noreply.github.com>
2020-07-19 11:53:01 -0400

    GL error output enabled

--- include/Macros.h
+++ include/Macros.h
@@ -25,12 +25,3 @@
 // There's nothing here. Avoid macros if possible, they may not be typesafe.
 // Prefer things like (static) inlines, constexprs, and whatnot. Sometimes you have no choice.
 // If that's the case, add it here.
-
-
-// GL error checking macro. This needs some rework and should ideally be actually used in the code...
-#define GL_DEBUG 1
-#if GL_DEBUG
-    #define GL_CHK_ERR(_w) _w; { int e = glGetError(); if (e) printf("GL Error on %s :: %d: %d\n", __FILE__,  __LINE__ ,e); };
-#else
-    #define GL_CHK_ERR(_w) _w
-#endif

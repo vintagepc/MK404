f0005b7f0666a5804a071457ba886b0738ed42a3
vintagepc <53943260+vintagepc@users.noreply.github.com>
2020-08-01 12:58:16 -0400

    add print fan to Bear

--- utility/MK3S_Bear.h
+++ utility/MK3S_Bear.h
@@ -32,20 +32,21 @@ class MK3S_Bear: public OBJCollection
 	public:
 		MK3S_Bear(bool bMMU):OBJCollection("Bear")
 		{
-			AddObject(ObjClass::Z, "assets/Bear/bear21_mk3s_simulator_x-axis.obj",0,-.127,0,CM_TO_M)->SetSwapMode(GLObj::SwapMode::YMINUSZ);
-			m_pE = AddObject(ObjClass::X, "assets/Bear/bear21_mk3s_simulator_e-axis_w-hotend-fan.obj",-0.130-0.182000, 0.318000, -0.186000,CM_TO_M);
+			AddObject(ObjClass::Z, "assets/bear21_mk3s_simulator_x-axis.obj",0,-.127,0,CM_TO_M)->SetSwapMode(GLObj::SwapMode::YMINUSZ);
+			m_pE = AddObject(ObjClass::X, "assets/bear21_mk3s_simulator_e-axis_w-hotend-fan.obj",-0.130-0.182000, 0.318000, -0.186000,CM_TO_M);
 			m_pE->SetSwapMode(GLObj::SwapMode::YMINUSZ);
-			AddObject(ObjClass::Y, "assets/Bear/bear21_mk3s_simulator_y-axis.obj",0,0,-.1175,CM_TO_M)->SetSwapMode(GLObj::SwapMode::YMINUSZ);
-			m_pBaseObj = AddObject(ObjClass::Fixed, "assets/Bear/bear21_mk3s_simulator_frame.obj", CM_TO_M);
+			m_pPFS = AddObject(ObjClass::Other, "assets/bear21_mk3s_simulator_print_fan.obj", CM_TO_M);
+			m_pPFS->SetSwapMode(GLObj::SwapMode::YMINUSZ);
+			AddObject(ObjClass::Y, "assets/bear21_mk3s_simulator_y-axis.obj",0,0,-.1175,CM_TO_M)->SetSwapMode(GLObj::SwapMode::YMINUSZ);
+			m_pBaseObj = AddObject(ObjClass::Fixed, "assets/bear21_mk3s_simulator_frame.obj", CM_TO_M);
 			m_pBaseObj->SetSwapMode(GLObj::SwapMode::YMINUSZ);
-			m_pKnob = AddObject(ObjClass::Other, "assets/Bear/bear21_mk3s_simulator_lcd_knob.obj",CM_TO_M);
-			m_pFan = AddObject(ObjClass::Other, "assets/Bear/bear21_mk3s_simulator_hotend-fan-blade.obj",CM_TO_M);
+			m_pKnob = AddObject(ObjClass::Other, "assets/bear21_mk3s_simulator_lcd_knob.obj",CM_TO_M);
+			m_pFan = AddObject(ObjClass::Other, "assets/bear21_mk3s_simulator_hotend-fan-blade.obj",CM_TO_M);
 			SetMaterialMode(GL_AMBIENT_AND_DIFFUSE); // These first ones only have Kd.
 			AddObject(ObjClass::PrintSurface, "assets/SSSheet.obj",-0.127000,0.236000,-0.170000);
 			AddObject(ObjClass::Media, "assets/SDCard.obj",-0.157000, 0.147000, -0.448998,MM_TO_M);
 			m_pEVis = AddObject(ObjClass::Other,"assets/Triangles.obj",MM_TO_M);
 			m_pPFan = AddObject(ObjClass::Other, "assets/Print-fan_rotor.obj");
-			// AddObject(ObjClass::E, "assets/X_AXIS.obj");
 		};
 
 		void SetupLighting() override
@@ -62,11 +63,11 @@ class MK3S_Bear: public OBJCollection
 
 		inline bool SupportsMMU() override { return false; }
 
-		inline void ApplyLCDTransform() { glTranslatef(-0.051000, 0.203000, 0.045); }
+		inline void ApplyLCDTransform() override { glTranslatef(-0.051000, 0.203000, 0.045); }
 
-		inline void ApplyPLEDTransform() {glTranslatef(-0.201000, -0.062000, -0.45);};
+		inline void ApplyPLEDTransform() override {glTranslatef(-0.201000, -0.062000, -0.45);};
 
-		inline void ApplyPrintTransform() { glTranslatef(-0.131000, 0.236000, 0.173000);};
+		inline void ApplyPrintTransform() override { glTranslatef(-0.131000, 0.236000, 0.173000);};
 
 		inline float GetScaleFactor() override { return m_pBaseObj->GetScaleFactor();};
 
@@ -116,7 +117,12 @@ class MK3S_Bear: public OBJCollection
 
 		virtual void DrawPFan(int iRotation) override
 		{
-			glTranslatef(-0.135000, 0.2780, -0.138000);
+			glTranslatef(-0.11100, 0.2710, -0.148000);
+			glPushMatrix();
+				glTranslatef(-0.03000, 0.0, -0.002000);
+				glRotatef(90,0,1,0);
+				m_pPFS->Draw();
+			glPopMatrix();
 			glRotatef(90,1,0,0);
 			glPushMatrix();
 				glRotatef((float)iRotation,0,1,0);
@@ -136,6 +142,6 @@ class MK3S_Bear: public OBJCollection
 		 	m_pEVis->Draw();
 		}
 
-		GLObj *m_pKnob = nullptr, *m_pFan = nullptr, *m_pEVis = nullptr, *m_pPFan = nullptr, *m_pE = nullptr;
+		GLObj *m_pKnob = nullptr, *m_pFan = nullptr, *m_pEVis = nullptr, *m_pPFan = nullptr, *m_pE = nullptr, *m_pPFS = nullptr;
 
 };

a76ed52b6154fe05112297a20c70af82062af2fc
Bozo Dragojevic <bozzo@digiverse.si>
2020-08-07 20:55:53 +0200

    add missing override

--- parts/components/PAT9125.h
+++ parts/components/PAT9125.h
@@ -222,7 +222,7 @@ class PAT9125: public I2CPeripheral, public Scriptable
 			}
 		};
 
-		virtual bool SetRegVal(uint8_t uiAddr, uint32_t uiData)
+		virtual bool SetRegVal(uint8_t uiAddr, uint32_t uiData) override
 		{
 			if (!(m_uiRW  & (0x01<<uiAddr)))
 			{

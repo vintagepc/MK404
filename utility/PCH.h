1a5a01619143279874b4c1f67306b88e0b9588f8
vintagepc <53943260+vintagepc@users.noreply.github.com>
2020-10-01 20:19:16 -0400

    add PCH for C STL and 3rdparty

---
+++ utility/PCH.h
@@ -0,0 +1,12 @@
+// PCH test file
+
+#include <atomic>
+#include <cstdint>
+#include <cstddef>
+#include <iostream>
+#include <iomanip>
+#include <map>
+#include <string>
+#include <utility>
+#include <vector>
+#include "gsl-lite.hpp"

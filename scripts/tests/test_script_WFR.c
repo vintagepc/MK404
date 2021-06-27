c6e0aec1e9138f67715d9db155af1cea44c096e1
VintagePC <53943260+vintagepc@users.noreply.github.com>
2021-06-27 11:59:03 -0400

    Add simple test case

---
+++ scripts/tests/test_script_WFR.c
@@ -0,0 +1,56 @@
+/*
+	atmega88_uart_echo.c
+
+	This test case enables uart RX interupts, does a "printf" and then receive characters
+	via the interupt handler until it reaches a \r.
+
+	This tests the uart reception fifo system. It relies on the uart "irq" input and output
+	to be wired together (see simavr.c)
+ */
+
+#include <avr/io.h>
+#include <stdio.h>
+#include <avr/interrupt.h>
+#include <avr/eeprom.h>
+#include <avr/sleep.h>
+#include <util/delay.h>
+
+/*
+ * This demonstrate how to use the avr_mcu_section.h file
+ * The macro adds a section to the ELF file with useful
+ * information for the simulator
+ */
+#include "avr_mcu_section.h"
+AVR_MCU(16000000, "atmega2560");
+
+#define PIN(x,y) PORT##x>>y & 1U
+
+volatile uint8_t done = 0;
+
+static int uart_putchar(char c, FILE *stream)
+{
+	loop_until_bit_is_set(UCSR0A, UDRE0);
+	UDR0 = c;
+	return 0;
+}
+static FILE mystdout = FDEV_SETUP_STREAM(uart_putchar, NULL,
+                                         _FDEV_SETUP_WRITE);
+int main()
+{
+	stdout = &mystdout;
+	sei();
+
+	printf("READY\n");
+
+
+	while(1){};
+
+	cli();
+
+
+	printf("FINISHED\n");
+
+	// this quits the simulator, since interupts are off
+	// this is a "feature" that allows running tests cases and exit
+	sleep_cpu();
+}
